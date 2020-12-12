5cd82ff669fbc846c76e26ec6cf39f42c3659ae4
polo-sec <mattswann@protonmail.com>
2020-12-12 13:39:00 +0000

    added dmenu

--- st/config.h
+++ st/config.h
@@ -97,7 +97,7 @@ unsigned int tabspaces = 8;
 static const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+  [0] = "#222222", /* hard contrast: #1d2021 / soft contrast: #32302f */
   [1] = "#cc241d", /* red     */
   [2] = "#98971a", /* green   */
   [3] = "#d79921", /* yellow  */
