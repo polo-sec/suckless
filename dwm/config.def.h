3043fe361fbb5881eae8dfd91d6ee76b55f6511e
polo-sec <mattswann@protonmail.com>
2020-12-13 16:01:01 +0000

    updated builds, added dmenu

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -11,7 +11,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#3c3836";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
